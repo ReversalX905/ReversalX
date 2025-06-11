5b11f82837dad63eeb1ffa269ff24391cb82b775
ReversalX905 <hyperservices13@gmail.com>
2025-06-11 08:37:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buyButton = document.getElementById('buyNowBtn');
+    if (buyButton) {
+        buyButton.addEventListener('click', () => {
+            window.location.href = 'https://t.me/ReversalX_Support';
+        });
+    }
+});
